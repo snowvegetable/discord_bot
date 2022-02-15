fa560473a1586dc511c2046cc493cace01f495ad
snowvegetable <55278536+snowvegetable@users.noreply.github.com>
2022-02-15 21:48:55 +0800

    添加 load unload reload commands 功能

--- bot.py
+++ bot.py
@@ -22,6 +22,25 @@ async def on_member_remove(member):
     print(f"{member} leave!")
 
 
+# load unload reload commands
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f'cmds.{extension}')
+    await ctx.send(f"loaded {extension}!")
+
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f'cmds.{extension}')
+    await ctx.send(f"unloaded {extension}!")
+
+
+@bot.command()
+async def reload(ctx, extension):
+    bot.reload_extension(f'cmds.{extension}')
+    await ctx.send(f"reloaded {extension}!")
+
+
 for FileName in os.listdir('./cmds'):
     if FileName.endswith('.py'):
         bot.load_extension(f"cmds.{FileName[:-3]}")
